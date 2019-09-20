3fe316dcdaaeb6916071e9c4f8ba1a5c7413ed77
MisterTicot <mister.ticot@cosmic.plus>
2019-09-20 20:26:12 +0800

    ui Fix: update address bar when navigating to anchor

--- src/helpers/url-view.js
+++ src/helpers/url-view.js
@@ -62,23 +62,17 @@ function rewriteLinks (iframe) {
     const isSameDomain = link.href.match(domainRegexp)
     if (isSameDomain) {
       const href = isSameDomain[1]
-      const [page, hash] = href.split("#", 2)
+      const [page] = href.split("#", 1)
       const pathname = iframe.contentWindow.location.pathname
 
       // `href` for copying link & navigation in new tab/window, `onclick` for
       // same-page nagivation.
-      if (pathname.substr(1) === page) {
-        // Anchor links.
-        link.href = `${domain}#view:${href.replace("#", "%23")}`
-        link.onclick = event => {
-          event.preventDefault()
-          idocument.location.hash = "#" + hash
-        }
-      } else {
-        // Page links.
-        const encodedHref = href.replace("#", "%23")
-        link.href = `${domain}#view:${encodedHref}`
-        link.onclick = () => location.hash = `#view:${encodedHref}`
+      const isSamePage = pathname.substr(1) === page
+      const encodedHref = href.replace("#", "%23")
+      link.href = `${domain}#view:${encodedHref}`
+      link.onclick = event => {
+        if (isSamePage) event.preventDefault()
+        location.hash = `#view:${encodedHref}`
       }
     } else if (link.href.substr(0, 1) !== "#") {
       link.target = "_blank"
